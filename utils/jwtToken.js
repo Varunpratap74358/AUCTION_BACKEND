b0cf223fe6afb01cde301dbdac7b8006e5854cc4
Varun Pratap Singh <145147806+Varunpratap74358@users.noreply.github.com>
2024-10-30 20:21:13 -0700

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,7 +4,9 @@ export const genrateToken = (user, message, statusCode, res) => {
       .status(statusCode)
       .cookie("token", token, {
         expires: new Date(Date.now() + 7 * 24 * 60 *60* 1000),
-        httpOnly:true
+        httpOnly:true,
+          secure:true,
+          sameSite:"None"
       })
       .json({
         success: true,
